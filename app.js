dc04a8c8193b9763a6878fc27fa7a2d25be81322
Chirag Parmar <95179516+chiragxp@users.noreply.github.com>
2025-04-25 00:10:58 -0400

    Implemented Error Handling

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+
 import { PORT } from "./config/env.js";
+
 import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.route.js";
+
 import connectToDatabase from "./database/mongodb.js";
 
+import errorMiddleware from "./middlewares/error.middleware.js";
+
+// Creating a constant of express.js - express is a function! (it is also an object, so it has properties too)
 const app = express();
 
+// express.json allows app to handle JSON data sent in requests or API calls
+app.use(express.json());
+
+// express.urlencoded helps to process data sent via HTML forms in a simple format
+app.use(express.urlencoded({ extended: false }));
+
+// cookieParser reads cookies from incoming requests so that app can store user data
+app.use(cookieParser());
+
+// URLs for Routers
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
+
+// Error Middleware to handle common errors
+app.use(errorMiddleware);
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Subscription Tracker API!");
